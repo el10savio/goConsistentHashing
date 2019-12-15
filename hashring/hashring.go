8fcd8a4dbebcbb300df879ed114731804e343f4f
Elton SV <el10savio96@gmail.com>
2019-12-15 12:53:07 +0530

    Added Comments

--- hashring/hashring.go
+++ hashring/hashring.go
@@ -5,29 +5,47 @@ import (
 	"sort"
 )
 
+// HashRing stores all the available
+// nodes that we can access
 type HashRing struct {
 	NodeList Nodes
 }
 
+// Nodes describes the composite
+// type of the slice of Nodes
 type Nodes []Node
 
+// Node stores the ID, usually the
+// IP address of the node and
+// its hash value
 type Node struct {
 	ID   string
 	Hash int
 }
 
+// The utility functions are
+// implemented so that
+// Nodes can be sorted
 func (nodes Nodes) Len() int           { return len(nodes) }
 func (nodes Nodes) Swap(i, j int)      { nodes[i], nodes[j] = nodes[j], nodes[i] }
 func (nodes Nodes) Less(i, j int) bool { return nodes[i].Hash < nodes[j].Hash }
 
+// HashValue returns the integer
+// hash using adler32
 func HashValue(value string) int {
 	return int(adler32.Checksum([]byte(value)))
 }
 
+// InitializeRing returns
+// an empty HashRing
 func InitializeRing() HashRing {
 	return HashRing{}
 }
 
+// InitializeNode returns a node with the
+// hash generated. In the case the ID is
+// not present it returns -1 as the
+// node's ID
 func InitializeNode(ID string) Node {
 	var newNode Node
 
@@ -43,10 +61,17 @@ func InitializeNode(ID string) Node {
 	return newNode
 }
 
+// Sort sorts the nodes in the HashRing
+// in ascending order based on
+// the node's hash value
 func (ring *HashRing) Sort() {
 	sort.Sort(ring.NodeList)
 }
 
+// IndexOfNode iterates through the HashRing
+// and returns the index of the node that
+// is searched. In the case the node is
+// not found it returns -1
 func (ring *HashRing) IndexOfNode(nodeToBeIndexed Node) int {
 	nodeIndex := -1
 
@@ -60,6 +85,8 @@ func (ring *HashRing) IndexOfNode(nodeToBeIndexed Node) int {
 	return nodeIndex
 }
 
+// InsertNode first appends the node to the
+// HashRing.NodeList and the sorts it
 func (ring *HashRing) InsertNode(node Node) {
 	if node.ID != "-1" {
 		ring.NodeList = append(ring.NodeList, node)
@@ -67,6 +94,9 @@ func (ring *HashRing) InsertNode(node Node) {
 	}
 }
 
+// RemoveNode searches for the node to be removed
+// and then if found removes it from
+// HashRing.NodeList
 func (ring *HashRing) RemoveNode(node Node) {
 	nodeIndex := ring.IndexOfNode(node)
 
@@ -76,7 +106,12 @@ func (ring *HashRing) RemoveNode(node Node) {
 	}
 }
 
-// AddValue Only tells which node to add to
+// AddValue returns the node in the HashRing that the
+// value can be added into based on its has. In the
+// case where the hash is greater than the last
+// node, it adds the value to the first node
+// present, thus looping through
+// all nodes like a ring
 func (ring *HashRing) AddValue(value string) Node {
 	entryHash := HashValue(value)
 
